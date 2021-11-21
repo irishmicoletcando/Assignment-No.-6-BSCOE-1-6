95079f0d733d8f271665df5dfc3ddbc9a089622c
Irish Micole Cando <irishmicolecando@gmail.com>
2021-11-21 21:50:11 +0800

    Added conditional statements for first number

--- Number_Sorter.py
+++ Number_Sorter.py
@@ -10,3 +10,25 @@ def ask_num():
 
     except ValueError:
         print("Enter a number.")
+
+    else:
+        # first number
+        if num1 > num2 and num1 > num3 and num1 > num4:
+            if num2 > num3 and num2 > num4:
+                if num3 > num4:
+                    descending = num1, num2, num3, num4
+                else:
+                    descending = num1, num2, num4, num3
+                return  descending
+            elif num3 > num2 and num3 > num4:
+                if num2 > num4:
+                    descending = num1, num3, num2, num4
+                else:
+                    descending = num1, num3, num4, num2
+                return descending
+            else:
+                if num3 > num2:
+                    descending = num1, num4, num3, num2
+                else:
+                    descending = num1, num4, num2, num3
+                return descending 
